e433dd57e1b9b426ab2608f63c73c0bdcee971ac
Fish shih <fish.shih@icloud.com>
2021-12-05 03:26:28 +0800

    [personalInfo] vm done

--- GithubUser/ViewModels/PersonalInfoViewModel.swift
+++ GithubUser/ViewModels/PersonalInfoViewModel.swift
@@ -8,15 +8,28 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import Combine
+
+// MARK: - Reaction
+
+enum PersonalInfoViewModelReaction {
+    case switchTab
+}
 
 // MARK: - Prototype
 
 protocol PersonalInfoViewModelInput {
-
+    func switchTab()
 }
 
 protocol PersonalInfoViewModelOutput {
-
+    var avatarURLString: AnyPublisher<String, Never> { get }
+    var name: AnyPublisher<String, Never> { get }
+    var loginID: AnyPublisher<String, Never> { get }
+    var numberOfFollowers: AnyPublisher<Int, Never> { get }
+    var numberOfFollowing: AnyPublisher<Int, Never> { get }
+    var email: AnyPublisher<String?, Never> { get }
+    var numberOfRepositories: AnyPublisher<Int, Never> { get }
 }
 
 protocol PersonalInfoViewModelPrototype {
@@ -28,8 +41,23 @@ protocol PersonalInfoViewModelPrototype {
 
 class PersonalInfoViewModel: PersonalInfoViewModelPrototype {
 
+    let reaction = PassthroughSubject<PersonalInfoViewModelReaction, Never>()
+
     var input: PersonalInfoViewModelInput { self }
     var output: PersonalInfoViewModelOutput { self }
+
+    init(api: UserInfoAPIPrototype?) {
+
+        self.api = api
+
+        guard let api = self.api else { return }
+
+        bind(api: api)
+    }
+
+    private let api: UserInfoAPIPrototype?
+
+    @Published private var model: UserModel?
 
     private let disposeBag = DisposeBag()
 }
@@ -38,14 +66,67 @@ class PersonalInfoViewModel: PersonalInfoViewModelPrototype {
 
 extension PersonalInfoViewModel: PersonalInfoViewModelInput {
 
+    func switchTab() {
+        reaction.send(.switchTab)
+    }
 }
 
 extension PersonalInfoViewModel: PersonalInfoViewModelOutput {
 
+    private var modelPublisher: AnyPublisher<UserModel, Never> {
+        $model
+            .compactMap { $0 }
+            .eraseToAnyPublisher()
+    }
+
+    var avatarURLString: AnyPublisher<String, Never> {
+        modelPublisher.map(\.avatarURL).eraseToAnyPublisher()
+    }
+
+    var name: AnyPublisher<String, Never> {
+        modelPublisher.map(\.name).eraseToAnyPublisher()
+    }
+
+    var loginID: AnyPublisher<String, Never> {
+        modelPublisher.map(\.login).eraseToAnyPublisher()
+    }
+
+    var numberOfFollowers: AnyPublisher<Int, Never> {
+        modelPublisher.map(\.followers).eraseToAnyPublisher()
+    }
+
+    var numberOfFollowing: AnyPublisher<Int, Never> {
+        modelPublisher.map(\.following).eraseToAnyPublisher()
+    }
+
+    var email: AnyPublisher<String?, Never> {
+        modelPublisher.map(\.email).eraseToAnyPublisher()
+    }
+
+    var numberOfRepositories: AnyPublisher<Int, Never> {
+        modelPublisher.map(\.publicRepos).eraseToAnyPublisher()
+    }
 }
 
 // MARK: - Private function
 
 private extension PersonalInfoViewModel {
 
+    func bind(api: UserInfoAPIPrototype) {
+
+        api
+            .result
+            .subscribe(onNext: {
+                [weak self] result in
+                switch result {
+                case .success(let model):
+                    self?.model = model
+                case .failure(let error):
+                    assert(false, "\(error)")
+                }
+            })
+            .disposed(by: disposeBag)
+
+        api.fetch()
+    }
 }
