c2ee376d02b2a89dd3fc8832b1491881d6c56414
Fish shih <fish.shih@icloud.com>
2021-12-05 01:55:01 +0800

    update UserInfoItemModel

--- GithubUser/Models/UserInfoItemModel.swift
+++ GithubUser/Models/UserInfoItemModel.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct UserInfoItemModel {
+enum ImageType {
+    case system(name: String)
+    case asset(name: String)
+}
 
-    enum ImageType {
-        case system(name: String)
-        case asset(name: String)
-    }
+struct UserInfoItemModel {
 
     let image: ImageType
     let value: String?
