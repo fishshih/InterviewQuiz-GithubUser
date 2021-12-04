9919185967f321da77530de7898253ba7aed9a10
Fish shih <fish.shih@icloud.com>
2021-12-05 01:29:19 +0800

    [userInfo] UserInfoItemTableViewCell done

--- GithubUser/Views/UserInfo/Subviews/UserInfoItemTableViewCell.swift
+++ GithubUser/Views/UserInfo/Subviews/UserInfoItemTableViewCell.swift
@@ -14,6 +14,8 @@ class UserInfoItemTableViewCell: UITableViewCell {
 
     // MARK: - Property
 
+    @Published var image: ImageType?
+    @Published var value: String?
 
     // MARK: - Life cycle
 
@@ -31,7 +33,15 @@ class UserInfoItemTableViewCell: UITableViewCell {
 
     // MARK: - Private property
 
+    private let iconImageView = UIImageView()
+
+    private let valueLabel = UILabel() --> {
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+        $0.textColor = #colorLiteral(red: 0.08759657294, green: 0.1211735532, blue: 0.1974031329, alpha: 1)
+    }
+
     private let disposeBag = DisposeBag()
+    private var cancelableSet = Set<AnyCancellable>()
 }
 
 // MARK: - UI configure
@@ -39,15 +49,36 @@ class UserInfoItemTableViewCell: UITableViewCell {
 private extension UserInfoItemTableViewCell {
 
     func setupUI() {
+        configureIconImageView()
+        configureValueLabel()
+    }
 
+    func configureIconImageView() {
+
+        contentView.addSubview(iconImageView)
+
+        iconImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(40)
+            $0.width.equalTo(40)
+        }
+    }
+
+    func configureValueLabel() {
+
+        contentView.addSubview(valueLabel)
+
+        valueLabel.snp.makeConstraints {
+            $0.centerY.equalTo(iconImageView)
+            $0.leading.equalTo(iconImageView.snp.trailing).offset(24)
+            $0.trailing.lessThanOrEqualToSuperview().offset(-40)
+        }
     }
 }
 
 // MARK: - Private func
 
-private extension UserInfoItemTableViewCell {
-
-}
+private extension UserInfoItemTableViewCell { }
 
 // MARK: - Binding
 
@@ -55,5 +86,18 @@ private extension UserInfoItemTableViewCell {
 
     func bind() {
 
+        $image
+            .map {
+                switch $0 {
+                case .system(name: let name):
+                    return UIImage(systemName: name)
+                case .asset(name: let name):
+                    return UIImage(named: name)
+                }
+            }
+            .assign(to: \.image, on: iconImageView)
+            .store(in: &cancelableSet)
+
+        $value.assign(to: \.text, on: valueLabel).store(in: &cancelableSet)
     }
 }
